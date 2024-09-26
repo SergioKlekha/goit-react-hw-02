3054789bc8d5672a797ea629903939c3b533fd34
RedDante <92797345+RedDante@users.noreply.github.com>
2024-09-26 11:30:09 +0100

    DZ

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-
-
-import Profile from './components/Profile/Profile';
-import FriendList from "./FriendList/FriendList";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
-import friends from "../friends.json";
-import userData from '../userData.json';
-import transactions from "../transaction.json";
-
-
-
+import Description from "./Description/Description";
 
 
 
 const App = () => {
   return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+    <Description/>
     </>
   );
 };
 
-export default App;
 
+export default App;
