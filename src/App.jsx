08949610eed745478746969c0c0c50ebc7bf4e8d
RedDante <92797345+RedDante@users.noreply.github.com>
2025-04-02 09:57:13 +0100

    fix dz

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Description from "./Description/Description";
-import Options from "./Options/Options";
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
 import React, { useState, useEffect } from "react";
-import Feedback from "./FeedBack/FeedBack";
-import Notification from "./Notification/Notification";
+import Feedback from "./components/FeedBack/FeedBack";
+import Notification from "./components/Notification/Notification";
 
 
 const App = () => {
